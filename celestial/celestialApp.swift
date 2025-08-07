a3a1413daae133193022df45906ec4b6355068fb
cranci1 <100066266+cranci1@users.noreply.github.com>
2025-08-07 11:41:45 +0200

    Initial Commit

---
+++ celestial/celestialApp.swift
@@ -0,0 +1,17 @@
+//
+//  celestialApp.swift
+//  celestial
+//
+//  Created by Francesco on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct celestialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
